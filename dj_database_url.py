d6a793f179347984806fca9e6d05256cd4a7c586
Alexander Gerbik <alexander.gerbik@tripledotstudios.com>
2022-02-17 11:52:07 +0300

    Make backend registration more generic
    Remove code addressing python2/python3 differences.
    Update README.
    Move deprecation handling to dedicated function/test case
    for simpler further removal.
    Deprecate 'backend' argument as it's possible to achieve the same result
    using "ENGINE" kwarg.

--- dj_database_url.py
+++ dj_database_url.py
@@ -1,82 +1,111 @@
-# -*- coding: utf-8 -*-
-
-import collections
 import os
 import urllib.parse as urlparse
-import warnings
 
-Engine = collections.namedtuple("Engine", ["backend", "string_ports", "options"])
-
-DEFAULT_ENV = "DATABASE_URL"
 ENGINE_SCHEMES = {}
 
 
-def register(backend, schemes=None, string_ports=False, options=None):
-    if schemes is None:
-        schemes = [backend.rsplit(".")[-1]]
-    elif isinstance(schemes, str):
-        schemes = [schemes]
+class ParseError(ValueError):
+    def __str__(self):
+        return (
+            "This string is not a valid url, possibly because some of its parts"
+            " is not properly urllib.parse.quote()'ed."
+        )
 
+
+class UnknownSchemeError(ValueError):
+    def __init__(self, scheme):
+        self.scheme = scheme
+
+    def __str__(self):
+        return (
+            f"Scheme '{self.scheme}://' is unknown."
+            " Did you forget to register custom backend?"
+        )
+
+
+def default_postprocess(config):
+    pass
+
+
+class Engine:
+    def __init__(self, backend, postprocess=default_postprocess):
+        self.backend = backend
+        self.postprocess = postprocess
+
+
+def register(backend, schemes=None):
+    engine = Engine(backend)
+    schemes = schemes or [backend.rsplit(".")[-1]]
+    schemes = [schemes] if isinstance(schemes, str) else schemes
     for scheme in schemes:
         urlparse.uses_netloc.append(scheme)
-        ENGINE_SCHEMES[scheme] = Engine(backend, string_ports, options or {})
+        ENGINE_SCHEMES[scheme] = engine
+
+    def inner(func):
+        engine.postprocess = func
+        return func
+
+    return inner
 
 
-# Support all the first-party Django engines out of the box.
-register(
-    "django.db.backends.postgresql",
-    ("postgres", "postgresql", "pgsql"),
-    options={
-        "currentSchema": lambda values: {
-            "options": "-c search_path={}".format(values[-1])
-        },
-    },
-)
-register(
-    "django.contrib.gis.db.backends.postgis",
-    options={
-        "currentSchema": lambda values: {
-            "options": "-c search_path={}".format(values[-1])
-        },
-    },
-)
 register("django.contrib.gis.db.backends.spatialite")
-register(
-    "django.db.backends.mysql",
-    options={
-        "ssl-ca": lambda values: {"ssl": {"ca": values[-1]}},
-    },
-)
 register("django.contrib.gis.db.backends.mysql", "mysqlgis")
-register("django.db.backends.oracle", string_ports=True)
 register("django.contrib.gis.db.backends.oracle", "oraclegis")
-register("django.db.backends.sqlite3", "sqlite")
 
 
-def config(env=DEFAULT_ENV, default=None, **settings):
-    """Returns configured DATABASE dictionary from DATABASE_URL."""
+@register("django.db.backends.sqlite3", "sqlite")
+def default_to_in_memory_db(config):
+    # mimic sqlalchemy behaviour
+    if config["NAME"] == "":
+        config["NAME"] = ":memory:"
 
+
+@register("django.db.backends.oracle")
+def stringify_port(config):
+    config["PORT"] = str(config["PORT"])
+
+
+@register("django.db.backends.mysql")
+def apply_ssl_ca(config):
+    options = config["OPTIONS"]
+    ca = options.pop("ssl-ca", None)
+    if ca:
+        options["ssl"] = {"ca": ca}
+
+
+@register("django.db.backends.postgresql", ("postgres", "postgresql", "pgsql"))
+@register("django.contrib.gis.db.backends.postgis")
+def apply_current_schema(config):
+    options = config["OPTIONS"]
+    schema = options.pop("currentSchema", None)
+    if schema:
+        options["options"] = f"-c search_path={schema}"
+
+
+def config(env="DATABASE_URL", default=None, **settings):
+    """
+    Gets a database URL from environmental variable named as 'env' value and parses it.
+    """
     s = os.environ.get(env, default)
     return parse(s, **settings) if s else {}
 
 
-def parse(url, backend=None, **settings):
-    """Parses a database URL."""
+def address_deprecated_arguments(backend, settings):
+    import warnings
 
-    if url == "sqlite://:memory:":
-        # this is a special case, because if we pass this URL into
-        # urlparse, urlparse will choke trying to interpret "memory"
-        # as a port number
-        return {"ENGINE": ENGINE_SCHEMES["sqlite"].backend, "NAME": ":memory:"}
-        # note: no other settings are required for sqlite
-
-    url = urlparse.urlparse(url)
-    engine = ENGINE_SCHEMES[url.scheme]
-    options = {}
+    if backend is not None:
+        message = (
+            "Using positional argument `backend`"
+            " to override database backend is deprecated."
+            " Use keyword argument `ENGINE` instead."
+        )
+        warnings.warn(message)
+        settings["ENGINE"] = backend
 
     if "engine" in settings:
-        # Keep compatibility with dj-database-url for `engine` kwarg.
-        backend = settings.pop("engine")
+        message = "The `engine` argument is deprecated. Use `ENGINE` instead."
+        warnings.warn(message)
+        settings["ENGINE"] = settings.pop("engine")
 
     if "conn_max_age" in settings:
         warnings.warn(
@@ -86,62 +115,58 @@ def parse(url, backend=None, **settings):
 
     if "ssl_require" in settings:
         warnings.warn(
-            "The `ssl_require` argument is deprecated. "
-            "Use `OPTIONS={'sslmode': 'require'}` instead."
+            "The `ssl_require` argument is deprecated."
+            " Use `OPTIONS={'sslmode': 'require'}` instead."
         )
         if settings.pop("ssl_require"):
+            options = settings.pop("OPTIONS", {})
             options["sslmode"] = "require"
+            settings["OPTIONS"] = options
+    return backend
 
-    # Split query strings from path.
-    path = url.path[1:]
-    if "?" in path and not url.query:
-        path, query = path.split("?", 1)
-    else:
-        path, query = path, url.query
-    query = urlparse.parse_qs(query)
 
-    # If we are using sqlite and we have no path, then assume we
-    # want an in-memory database (this is the behaviour of sqlalchemy)
-    if url.scheme == "sqlite" and path == "":
-        path = ":memory:"
+def parse(url, backend=None, **settings):
+    """Parses a database URL and returns configured DATABASE dictionary."""
 
-    # Handle postgres percent-encoded paths.
-    hostname = url.hostname or ""
-    if "%2f" in hostname.lower():
-        # Switch to url.netloc to avoid lower cased paths
-        hostname = url.netloc
-        if "@" in hostname:
-            hostname = hostname.rsplit("@", 1)[1]
-        if ":" in hostname:
-            hostname = hostname.split(":", 1)[0]
-        hostname = hostname.replace("%2f", "/").replace("%2F", "/")
+    address_deprecated_arguments(backend, settings)
 
-    port = str(url.port) if url.port and engine.string_ports else url.port
+    if url == "sqlite://:memory:":
+        # this is a special case, because if we pass this URL into
+        # urlparse, urlparse will choke trying to interpret "memory"
+        # as a port number
+        return {"ENGINE": ENGINE_SCHEMES["sqlite"].backend, "NAME": ":memory:"}
+        # note: no other settings are required for sqlite
 
-    # Pass the query string into OPTIONS.
-    for key, values in query.items():
-        if key in engine.options:
-            options.update(engine.options[key](values))
-        else:
-            options[key] = values[-1]
+    try:
+        url = urlparse.urlparse(url)
+        engine = ENGINE_SCHEMES.get(url.scheme)
+        if engine is None:
+            raise UnknownSchemeError(url.scheme)
+        path = url.path[1:]
+        query = urlparse.parse_qs(url.query)
+        options = {k: (v[0] if len(v) == 1 else v) for k, v in query.items()}
+        config = {
+            "ENGINE": engine.backend,
+            "USER": urlparse.unquote(url.username or ""),
+            "PASSWORD": urlparse.unquote(url.password or ""),
+            "HOST": urlparse.unquote(url.hostname or ""),
+            "PORT": url.port or "",
+            "NAME": urlparse.unquote(path),
+            "OPTIONS": options,
+        }
+    except UnknownSchemeError:
+        raise
+    except ValueError:
+        raise ParseError() from None
 
-    # Allow passed OPTIONS to override query string options.
-    options.update(settings.pop("OPTIONS", {}))
-
-    # Update with environment configuration.
-    config = {
-        "ENGINE": backend or engine.backend,
-        "NAME": urlparse.unquote(path or ""),
-        "USER": urlparse.unquote(url.username or ""),
-        "PASSWORD": urlparse.unquote(url.password or ""),
-        "HOST": hostname,
-        "PORT": port or "",
-    }
-
-    if options:
-        config["OPTIONS"] = options
+    # Guarantee that config has options, possibly empty, when postprocess() is called
+    assert isinstance(config["OPTIONS"], dict)
+    engine.postprocess(config)
 
     # Update the final config with any settings passed in explicitly.
+    config["OPTIONS"].update(settings.pop("OPTIONS", {}))
     config.update(**settings)
 
+    if not config["OPTIONS"]:
+        config.pop("OPTIONS")
     return config
--- test_dj_database_url.py
+++ test_dj_database_url.py
@@ -1,29 +1,119 @@
-# -*- coding: utf-8 -*-
-#!/usr/bin/env python
-
 import os
 import unittest
 
 import dj_database_url
 
-POSTGIS_URL = "postgis://uf07k1i6d8ia0v:wegauwhgeuioweg@ec2-107-21-253-135.compute-1.amazonaws.com:5431/d8r82722r2kuvn"
+URL = "postgres://user:password@localhost/db-name"
 EXPECTED_POSTGRES_ENGINE = "django.db.backends.postgresql"
 
 # These were supported out of the box in dj-database-url.
 dj_database_url.register("mysql.connector.django", "mysql-connector")
-dj_database_url.register("sql_server.pyodbc", "mssql", string_ports=True)
-dj_database_url.register(
-    "django_redshift_backend",
-    "redshift",
-    options={
-        "currentSchema": lambda values: {
-            "options": "-c search_path={}".format(values[-1])
-        },
-    },
+dj_database_url.register("sql_server.pyodbc", "mssql")(dj_database_url.stringify_port)
+dj_database_url.register("django_redshift_backend", "redshift")(
+    dj_database_url.apply_current_schema
 )
 
 
+class DeprecatedArgumentsTestSuite(unittest.TestCase):
+    def test_config_conn_max_age_setting(self):
+        conn_max_age = 600
+        os.environ[
+            "DATABASE_URL"
+        ] = "mysql://bea6eb025ca0d8:69772142@us-cdbr-east.cleardb.com/heroku_97681db3eff7580?reconnect=true"
+        message = (
+            "The `conn_max_age` argument is deprecated. Use `CONN_MAX_AGE` instead."
+        )
+        with self.assertWarns(Warning, msg=message):
+            url = dj_database_url.config(conn_max_age=conn_max_age)
+
+        assert url["CONN_MAX_AGE"] == conn_max_age
+        del os.environ["DATABASE_URL"]
+
+    def test_parse_conn_max_age_setting(self):
+        conn_max_age = 600
+        url = "mysql://bea6eb025ca0d8:69772142@us-cdbr-east.cleardb.com/heroku_97681db3eff7580?reconnect=true"
+        message = (
+            "The `conn_max_age` argument is deprecated. Use `CONN_MAX_AGE` instead."
+        )
+        with self.assertWarns(Warning, msg=message):
+            url = dj_database_url.parse(url, conn_max_age=conn_max_age)
+
+        assert url["CONN_MAX_AGE"] == conn_max_age
+
+    def test_config_engine_setting(self):
+        engine = "django_mysqlpool.backends.mysqlpool"
+        os.environ[
+            "DATABASE_URL"
+        ] = "mysql://bea6eb025ca0d8:69772142@us-cdbr-east.cleardb.com/heroku_97681db3eff7580?reconnect=true"
+        message = "The `engine` argument is deprecated. Use `ENGINE` instead."
+        with self.assertWarns(Warning, msg=message):
+            url = dj_database_url.config(engine=engine)
+
+        assert url["ENGINE"] == engine
+        del os.environ["DATABASE_URL"]
+
+    def test_parse_engine_setting(self):
+        engine = "django_mysqlpool.backends.mysqlpool"
+        url = "mysql://bea6eb025ca0d8:69772142@us-cdbr-east.cleardb.com/heroku_97681db3eff7580?reconnect=true"
+        message = (
+            "Using positional argument `backend`"
+            " to override database backend is deprecated."
+            " Use keyword argument `ENGINE` instead."
+        )
+        with self.assertWarns(Warning, msg=message):
+            url = dj_database_url.parse(url, engine)
+
+        assert url["ENGINE"] == engine
+
+
 class DatabaseTestSuite(unittest.TestCase):
+    def test_credentials_unquoted__raise_value_error(self):
+        expected_message = (
+            "This string is not a valid url, possibly because some of its parts "
+            r"is not properly urllib.parse.quote()'ed."
+        )
+        with self.assertRaises(ValueError, msg=expected_message):
+            dj_database_url.parse("postgres://user:passw#ord!@localhost/foobar")
+
+    def test_credentials_quoted__ok(self):
+        url = "postgres://user%40domain:p%23ssword!@localhost/foobar"
+        config = dj_database_url.parse(url)
+        assert config["USER"] == "user@domain"
+        assert config["PASSWORD"] == "p#ssword!"
+
+    def test_unknown_scheme__raise_value_error(self):
+        expected_message = "Scheme 'unknown-scheme://' is unknown. Did you forget to register custom backend?"
+        with self.assertRaises(ValueError, msg=expected_message):
+            dj_database_url.parse("unknown-scheme://user:password@localhost/foobar")
+
+    def test_provide_test_settings__add_them_to_final_config(self):
+        settings = {
+            "TEST": {
+                "NAME": "mytestdatabase",
+            },
+        }
+        config = dj_database_url.parse(URL, **settings)
+        assert config["TEST"] == {"NAME": "mytestdatabase"}
+
+    def test_provide_options__add_them_to_final_config(self):
+        options = {"options": "-c search_path=other_schema"}
+        config = dj_database_url.parse(URL, OPTIONS=options)
+        assert config["OPTIONS"] == options
+
+    def test_provide_clashing_options__use_options_from_kwargs(self):
+        options = {"reconnect": "false"}
+        config = dj_database_url.parse(f"{URL}?reconnect=true", OPTIONS=options)
+        assert config["OPTIONS"]["reconnect"] == "false"
+
+    def test_provide_custom_engine__use_it_in_final_config(self):
+        engine = "django_mysqlpool.backends.mysqlpool"
+        config = dj_database_url.parse(URL, ENGINE=engine)
+        assert config["ENGINE"] == engine
+
+    def test_provide_conn_max_age__use_it_in_final_config(self):
+        config = dj_database_url.parse(URL, CONN_MAX_AGE=600)
+        assert config["CONN_MAX_AGE"] == 600
+
     def test_postgres_parsing(self):
         url = "postgres://uf07k1i6d8ia0v:wegauwhgeuioweg@ec2-107-21-253-135.compute-1.amazonaws.com:5431/d8r82722r2kuvn"
         url = dj_database_url.parse(url)
@@ -146,7 +236,6 @@ class DatabaseTestSuite(unittest.TestCase):
         assert url["PORT"] == ""
 
     def test_database_url(self):
-        del os.environ["DATABASE_URL"]
         a = dj_database_url.config()
         assert not a
 
@@ -176,38 +265,6 @@ class DatabaseTestSuite(unittest.TestCase):
 
         assert url["ENGINE"] == "django.db.backends.sqlite3"
         assert url["NAME"] == ":memory:"
-
-    def test_parse_engine_setting(self):
-        engine = "django_mysqlpool.backends.mysqlpool"
-        url = "mysql://bea6eb025ca0d8:69772142@us-cdbr-east.cleardb.com/heroku_97681db3eff7580?reconnect=true"
-        url = dj_database_url.parse(url, engine)
-
-        assert url["ENGINE"] == engine
-
-    def test_config_engine_setting(self):
-        engine = "django_mysqlpool.backends.mysqlpool"
-        os.environ[
-            "DATABASE_URL"
-        ] = "mysql://bea6eb025ca0d8:69772142@us-cdbr-east.cleardb.com/heroku_97681db3eff7580?reconnect=true"
-        url = dj_database_url.config(engine=engine)
-
-        assert url["ENGINE"] == engine
-
-    def test_parse_conn_max_age_setting(self):
-        conn_max_age = 600
-        url = "mysql://bea6eb025ca0d8:69772142@us-cdbr-east.cleardb.com/heroku_97681db3eff7580?reconnect=true"
-        url = dj_database_url.parse(url, conn_max_age=conn_max_age)
-
-        assert url["CONN_MAX_AGE"] == conn_max_age
-
-    def test_config_conn_max_age_setting(self):
-        conn_max_age = 600
-        os.environ[
-            "DATABASE_URL"
-        ] = "mysql://bea6eb025ca0d8:69772142@us-cdbr-east.cleardb.com/heroku_97681db3eff7580?reconnect=true"
-        url = dj_database_url.config(conn_max_age=conn_max_age)
-
-        assert url["CONN_MAX_AGE"] == conn_max_age
 
     def test_database_url_with_options(self):
         # Test full options
